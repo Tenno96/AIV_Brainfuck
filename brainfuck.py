fc4b9633177db2b9cf12d6173bb72dd01638b99d
Francesco Fusco <francescofusco1996@gmail.com>
2025-11-07 08:59:12 +0100

    Accept .bf file Add help command Add validation file and check if there not allowed characters

--- brainfuck.py
+++ brainfuck.py
@@ -1,19 +1,20 @@
-BRAINFUCK_FILE_PATH = "brainfuck_file.txt"
+import os
 
 data_pointer = {}
 current_cell_index = 0
 
-def run_interpreter():
-    with open(BRAINFUCK_FILE_PATH, "r") as my_handle:
+def run_interpreter(file_path):
+    with open(file_path, "r") as my_handle:
        jump_map = {}
 
        file_str = my_handle.read()
-       jump_map = build_jump_map(file_str)
-       instruction_pointer = 0
+       if validation_brainfuck_file(file_str):
+        jump_map = build_jump_map(file_str)
+        instruction_pointer = 0
 
-       while instruction_pointer < len(file_str):
-           command = file_str[instruction_pointer]
-           instruction_pointer = handle_commad(command, jump_map, instruction_pointer)
+        while instruction_pointer < len(file_str):
+            command = file_str[instruction_pointer]
+            instruction_pointer = handle_commad(command, jump_map, instruction_pointer)
 
 
 def build_jump_map(file_str):
@@ -71,5 +72,71 @@ def handle_commad(command, jump_map, instruction_pointer):
 
     return next_ip
 
+def validation_brainfuck_file(file_string):
+
+    # Check file path exist
+    if not os.path.exists(final_file_path):
+        print(f"ERROR: The file {final_file_path} does not exist, please enter the name of an existing file.")      
+        return False
+    
+    # Check if there not allowed characters
+    valid_comands = ['>', '<', '+', '-', '.', '.', '[', ']']
+
+    for char in file_string:
+        if char not in valid_comands:
+            print(f"ERROR: The file {final_file_path} contains the character '{char}' which is not acceptable for the Brainfuck language")
+            return False
+        
+    return True
+
+def ask_file_name():
+    while True:
+        file_name = input("Enter file name (ex, hello or hello.bf): ")
+
+        # Display brainfuck commands.
+        if file_name.lower() == "help":
+            print("\n\nBrainfuck is an esoteric programming language with only 8 commands:\n" \
+                  ">   moves the memory pointer one cell to the right\n" \
+                  "<   moves the memory pointer one cell to the left\n" \
+                  "+   increments the value in the current cell\n" \
+                  "-   decrements the value in the current cell\n" \
+                  ".   outputs the ASCII character corresponding to the value in the current cell\n" \
+                  ",   reads one character of input and stores its ASCII value in the current cell\n" \
+                  "[   if the value in the current cell is 0, jumps forward to the command after the matching ']'\n" \
+                  "]   if the value in the current cell is not 0, jumps back to the command after the matching '['\n\n")
+            continue
+
+        final_file_path = file_name 
+
+        if not os.path.splitext(file_name)[1]:
+            print(f"File name '{file_name}' has no extension.")
+            print("which type file are you running?")
+            print("1: Brainfuck (.bf)")
+            print("2. Text (.txt)")
+    
+            while True:
+                choice_file_type = input("Enter 1 or 2 ").strip()
+    
+                if choice_file_type == '1':
+                    final_file_path = f"{file_name}.bf"
+                    break
+                elif choice_file_type == '2':
+                    final_file_path = f"{file_name}.txt"
+                    break
+                else:
+                    print("Invalid choice!")                       
+
+
+        # Check file existence
+
+
+        return final_file_path
+
+
 if __name__ == "__main__":
-    run_interpreter()
+    print("Hi, this is interpeter for the brainfuck lenguage. ")
+    final_file_path = ask_file_name()
+
+    print("\n--- Starting Brainfuck Interperter ---")
+    run_interpreter(final_file_path)
+    print("\n--- Execution finished ---")
